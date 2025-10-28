c7f7620feb7a74a23c2b0f4641923a2010a50892
mudit0829 <mekasutechnology@gmail.com>
2025-10-28 11:23:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,5 +70,8 @@ def admin_panel(admin_id):
         return jsonify({"message": "Admin not found"}), 404
     return jsonify(list(users.values()))
 
+import os
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
